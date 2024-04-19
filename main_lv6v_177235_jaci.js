802a38b82fa0489a14b637aa9e221c23e0cfd1f6
VasylTur <turliunvasyl@gmail.com>
2024-04-19 19:17:45 +0300

    Fixed dependencies

---
+++ main_lv6v_177235_jaci.js
@@ -0,0 +1,5 @@
+54,81,24,5,81,47,36,35,26,88,15,5,70,82,23 - true
+const calculateFactorial = n => n === 0 ? 1 : n * calculateFactorial(n - 1);
+
+false + true
+const findLargestNumber = numbers => Math.max(...numbers);
